170ba65b7fe6989e42b6921b1af45bc890ae4c5f
梓鹏 <44833128+961163289@users.noreply.github.com>
2022-05-08 23:45:33 +0800

    first submit

---
+++ src/main/java/类与对象/Object03.java
@@ -0,0 +1,24 @@
+package 类与对象;
+
+public class Object03 {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.age = 10;
+        p1.name = "小明";
+        Person p2 = p1; //把 p1 赋给了 p2, 让 p2 指向 p1
+        p1.age = 80;
+        System.out.println(p2.age);
+
+//        Person a = new Person();
+//        a.age = 10;
+//        a.name = "小明";
+//        Person b = a;
+//        System.out.println(b.name);//小明
+//        b.age = 200;
+//        b = null;
+//        System.out.println(a.age); //200
+//        System.out.println(b.age); //NullPointerException
+    }
+}
+
+
