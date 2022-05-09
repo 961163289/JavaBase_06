29888f17ed37bd030da43ab77d4589a014b92396
梓鹏 <44833128+961163289@users.noreply.github.com>
2022-05-09 21:09:21 +0800

    zi.peng

--- src/main/java/方法/MethodExercise01.java
+++ src/main/java/方法/MethodExercise01.java
@@ -6,7 +6,7 @@ public class MethodExercise01 {
         AAA a = new AAA();
 
         int num = 15;
-        if (a.isOdd(num)) {
+        if (a.isOdd(num)) {//T,这样的写法以后会看到很多
             System.out.println(num + " 是奇数");
         } else {
             System.out.println(num + " 是偶数");
@@ -25,11 +25,15 @@ class AAA {
     //3. 方法的形参 (int num)
     //4. 方法体 , 判断
     public boolean isOdd(int num) {
-        if (num % 2 != 0) {
-            return true;
-        } else {
-            return false;
-        }
+//        if (num % 2 != 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+
+//        return num % 2 != 0 ? true : false;
+
+        return num % 2 != 0;
     }
 
     //2)	根据行、列、字符打印 对应行数和列数的字符，比如：行：4，列：4，字符#,则打印相应的效果
@@ -37,13 +41,13 @@ class AAA {
     //1. 方法的返回类型 void
     //2. 方法的名字 print
     //3. 方法的形参 (int row, int col, char c)
-    //4. 方法体 , 循环
+    //4. 方法体, 循环
     public void printArray(int row, int col, char c) {
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+            for (int j = 0; j < col; j++) {//输出每一行
                 System.out.print(c + "\t");
             }
-            System.out.println();
+            System.out.println();//换行
         }
     }
 }
--- src/main/java/方法/MethodParameter02.java
+++ src/main/java/方法/MethodParameter02.java
@@ -16,7 +16,7 @@ public class MethodParameter02 {
         p.name = "jack";
         p.age = 10;
         b.test200(p);
-        //测试题, 如果 test200  执行的是 p = null ,下面的结果是 10
+        //测试题, 如果 test200  执行的是 p = null ,下面的结果是 10  一条线断了,另一条线还连着
         //测试题, 如果 test200  执行的是 p = new Person();..., 下面输出的是 10
         System.out.println("main的 p.age = " + p.age + ", p.name = " + p.name);
     }
